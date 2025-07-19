1b8585c098ff131934476a0554bddcabff7ec017
Huzaifa <huzaifahameedd@gmail.com>
2025-07-19 20:07:33 +0500

    Fix: Updated contact form with encrypted FormSubmit action

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ themeToggle.addEventListener('change', () => {
 
 const form = document.getElementById("contact-form")
 form.addEventListener("submit", function(e) {
-    e.preventDefault();
 
 const name = document.getElementById("name").value.trim();
 const email = document.getElementById("email").value.trim();
